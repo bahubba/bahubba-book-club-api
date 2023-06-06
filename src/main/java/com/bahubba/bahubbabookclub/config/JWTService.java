d8d559ef8f7c72bf1622c69ec719e5a8f5964a65
bahubba <bahubba89@gmail.com>
2023-06-06 17:39:24 -0400

    Fixed comment

--- src/main/java/com/bahubba/bahubbabookclub/config/JWTService.java
+++ src/main/java/com/bahubba/bahubbabookclub/config/JWTService.java
@@ -35,7 +35,7 @@ public class JWTService {
             .setClaims(extraClaims)
             .setSubject(userDetails.getUsername())
             .setIssuedAt(new Date(System.currentTimeMillis()))
-            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24)) // TODO - Determine how expiry length
+            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24)) // TODO - Determine expiry length
             .signWith(getSignInKey(), SignatureAlgorithm.HS256)
             .compact();
     }
